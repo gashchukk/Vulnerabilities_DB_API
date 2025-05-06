9eb334bc5fe5b1ce154d7cb4ae2cec743c500e1c
Богдан Гащук <hashchuk.pn@ucu.edu.ua>
2025-05-07 02:51:53 +0300

    black formatted

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,15 @@
 import pytest
 from fastapi.testclient import TestClient
-from main import app  
+from main import app
 
 client = TestClient(app)
+
 
 def test_status_endpoint():
     response = client.get("/status")
     assert response.status_code == 200
     assert "count" in response.json()
+
 
 def test_cve_from_nvd_real():
     response = client.get("/cve_from_nvd", params={"cve_id": "CVE-2021-34527"})
